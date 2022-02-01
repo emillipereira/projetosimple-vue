725a7a733fc905b0f97eae205273c3517bfb47a3
emillipereira <98535441+emillipereira@users.noreply.github.com>
2022-01-31 21:10:57 -0300

    Add files via upload

---
+++ Projeto simple - VUE/src/views/Home.vue
@@ -0,0 +1,47 @@
+<template>
+  <v-containte class="pa-8" fluid>
+    <h1 class="h1">My to-do list</h1>
+    <v-form>
+      <v-text-field 
+        label="Nova tarefa" 
+        v-model="novaTarefa" 
+        @keydown.enter="adicionar"
+      >
+        <v-icon slot="append" @click="adicionar">mdi-send</v-icon>
+      </v-text-field>
+    </v-form>
+    <v-divider></v-divider>
+    <v-list>
+      <v-list-item-group>
+        <v-list-item v-for="tarefa of tarefas" :key="tarefa.titulo">
+          {{tarefa.titulo}}
+        </v-list-item>
+      </v-list-item-group>
+    </v-list>
+  </v-containte>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      novaTarefa: '',
+      tarefas: [
+        {titulo: "Tarefa A"},
+        {titulo: "Tarefa B"},
+        {titulo: "Tarefa C"},
+      ],
+    };
+  },
+  methods: {
+    adicionar(e) {
+      e.preventDefault();
+      this.tarefas.push({titulo: this.novaTarefa});
+      this.novaTarefa = "";
+    },
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ Projeto simple - VUE/src/views/Perfil.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-container class="pa-8" fluid>
+    <h1 class="h1"> My profile </h1>
+    <v-form>
+      <v-container>
+        <v-text-field label="Nome" v-model="nome"></v-text-field>
+        <v-text-field label="Sobrenome" v-model="sobrenome"></v-text-field>
+        <v-btn color="secondary">To Save</v-btn>
+      </v-container>
+    </v-form>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      nome: '',
+      sobrenome: '',
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
